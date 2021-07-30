dde24eb3389d312101bdd6df66b8fd9c8fad298d
ihateuu <replacement506@gmail.com>
2021-07-30 15:20:21 +0300

    Initial Commit

---
+++ Calling APIs/Calling_APIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calling_APIsApp.swift
+//  Calling APIs
+//
+//  Created by Kareem Almomen on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Calling_APIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
