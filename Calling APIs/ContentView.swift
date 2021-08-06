d8051277ec89cde976193d3de0ffded3a7350fdb
ihateuu <replacement506@gmail.com>
2021-08-06 08:59:19 +0300

    Initial

--- Calling APIs/ContentView.swift
+++ Calling APIs/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+  
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(singles) {single in
+                NavigationLink(
+                    destination: VStack {
+                        Text(single.rank)
+                        Text(single.title)
+                        Text(single.artist)
+                        Text("Last Week: \(single.lastweek)")
+                        Text("Peak position: \(single.peakPosition)")
+                        Text("Weeks on chart: \(single.detail)")
+                        Text("Detail: \(single.detail)")
+                    },
+                    label: {
+                        HStack {
+                            
+                        }
+                    }
+        }
+    
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
